09ee8cc3cbf82ca911894ab19f7ad30adff2d9b6
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-05 21:29:48 -0400

    Update EJR_pytorch_tutorial.py

--- EJR_pytorch_tutorial.py
+++ EJR_pytorch_tutorial.py
@@ -13,3 +13,21 @@ print(f"Ones Tensor: \n {x_ones} \n")
 
 x_rand = torch.rand_like(x_data, dtype=torch.float) #overrides the datatype of x_data
 print(f"Random Tensor: \n {x_rand} \n")
+
+#create a tensor with random or constant values
+#shape is a tuple of tensor dimensions
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
+
+#Attributes of tensor to descirbe shape, datatype, and device on which they are stored
+tensor = torch.rand(3,4)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dytype}")
+print(f"Device tensor is stored on: {tensor.device}")
+#use torch.cat to concatenate a seq of tensors along given dimension
+
