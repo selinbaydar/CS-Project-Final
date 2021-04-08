c7c7b0b2e5b94a525a97112bc68c30edf2c7e315
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-07 20:27:12 -0400

    Update EJR_pytorch_tutorial.py

--- EJR_pytorch_tutorial.py
+++ EJR_pytorch_tutorial.py
@@ -27,7 +27,29 @@ print(f"Zeros Tensor: \n {zeros_tensor}")
 #Attributes of tensor to descirbe shape, datatype, and device on which they are stored
 tensor = torch.rand(3,4)
 print(f"Shape of tensor: {tensor.shape}")
-print(f"Datatype of tensor: {tensor.dytype}")
+print(f"Datatype of tensor: {tensor.dtype}")
 print(f"Device tensor is stored on: {tensor.device}")
+
 #use torch.cat to concatenate a seq of tensors along given dimension
+ 
+ #datasets and Data loaders Tutorial
+#two data primitives: torch.utils.data.DataLoader and torch.utils.data.Dataset which preload datasets
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+import matplotlib.pyplot as plt
+
+training_data = datasets.FashionMNIST(
+    root = "data",
+    train = True,
+    download = True,
+    transform=ToTensor()
+)
+
+test_data=datasets.FashionMNIST(
+    root = "data",
+    train = False,
+    download = True,
+    transform=ToTensor()
+)
 
