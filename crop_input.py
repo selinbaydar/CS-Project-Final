f7cd2d0d708176dd07fa56d488bee72d2d98c8cc
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-09 11:58:48 -0400

    starting to think about how to write this code

---
+++ crop_input.py
@@ -0,0 +1,11 @@
+#EJR wrote this code
+#EJR spent 30 minutes writing puesdo code on 4/9,
+
+#define a funciton that takes a folder full of images, then loops through each item in the folder 
+#for each image, normalize it first? or at least normalize the size so that the scale of what we zoom in on is the same
+#zoom in on the center of the image, feed image to alexnet (or other moel) and if alexnet can give the correct answer, the  size of the zoomed in image is the output
+#if alexnet cannot give the correct answer, zoom out slightly, repeat until alexnet can identify what the image is
+
+
+#Questions:
+#
