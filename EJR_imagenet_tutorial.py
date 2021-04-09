5df2e2eebaab68e71f32d00d8b2e7ce566932f5a
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-09 11:28:43 -0400

    finally got alexnet to identify the dog correctly!

--- EJR_imagenet_tutorial.py
+++ EJR_imagenet_tutorial.py
@@ -4,8 +4,10 @@
 #step1: loading pre-trained network using Torchvision
 import torchvision.models as models
 import torch
+import matplotlib.pyplot as plt
+
 #step2: load the pre-trained model 
-alexnet = models.alexnet()
+alexnet = models.alexnet(pretrained=True)
 #the purpose of alexnet is to take an image and predict it's class
 # QUESTION: what is happening with the directory for the cache?
 
@@ -28,16 +30,23 @@ transform = transforms.Compose([
     mean=[0.485,0.456,0.406],
     std=[0.229,0.224,0.225]
     )])
+
 #step4: load input image and pre-process it
 from PIL import Image
 img=Image.open("C:/Users/emmar/Documents/CLPS0950/CS-Project-Final/dog.jpg")
+
 #pre-process image and prepare a batch to be passed through network
-img_t=transform(img)
-batch_t=torch.unsqueeze(img_t,0)
+img_t = transform(img)
+
+plt.imshow(img_t[0])
+plt.show()
+
+batch_t = torch.unsqueeze(img_t,0)
+print(batch_t.shape)
 
 #step5: model inferenc, use pre-trained model to see what model thinks of image
 alexnet.eval() #put model in evaluation mode
-out=alexnet(batch_t)
+out = alexnet(batch_t)
 print(out.shape)
 
 #read and store labels from a text file that has all 1000 labels
@@ -45,7 +54,7 @@ with open('imagenet_classes.txt') as f:
     labels=[line.strip() for line in f.readlines()]
 
 #find out index where max score output vector occur, this is the index that will be used to make the prediction
-_,index=torch.max(out,1)
+_ , index = torch.max(out,1)
 percentage = torch.nn.functional.softmax(out,dim=1)[0]*100
 
 print(labels[index[0]],percentage[index[0]].item())
@@ -53,5 +62,5 @@ print(labels[index[0]],percentage[index[0]].item())
 
 #see what other classes the model thought the image belonged to
 _, indices = torch.sort(out, descending=True)
-[(labels[idx], percentage[idx].item()) for idx in indices[0][:5]]
+print([(labels[idx], percentage[idx].item()) for idx in indices[0][:5]])
 
