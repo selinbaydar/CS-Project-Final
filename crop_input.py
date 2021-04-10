2ffa8264dfcd9a4cae799a981665239553bc9816
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-10 10:31:43 -0400

    workspace not working again..

--- crop_input.py
+++ crop_input.py
@@ -10,11 +10,47 @@
 #Questions:
 #how can we integrate this with other functions we'll write?
 import os
-my_directory = r'C:\Users\emmar\Documents\CLPS0950\CS-Project-Final' #i want this to be given in the terminal instead..
+
 def crop(my_directory):
+    my_directory = 'C:\Users\emmar\Downloads\pretend' #i want this to be given in the terminal instead..
+    print pwd
+    if os.path.exists(my_directory)
+        print('yes')
     for filename in os.listdir(my_directory):
         if filename.endswith(".jpg")
-            print(os.path.join(directory, filename))
-        else:
-            continue
+            image_counter=os.path.join(my_directory, filename)
+            print(os.path.join(my_directory, filename))
+#         else:
+#             continue
+#         from PIL import Image
+#         img=Image.open(image_counter)
+
+#         from torchvision import transforms
+#             transform = transforms.Compose([transforms.Resize(256),transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])])
+#         img_t = transform(img)
+#         plt.imshow(img_t[0])
+#         plt.show()
+#         batch_t = torch.unsqueeze(img_t,0)
+#         print(batch_t.shape)
+        
+#         alexnet.eval() #put model in evaluation mode
+#         out = alexnet(batch_t)
+# print(out.shape)
+
+# #read and store labels from a text file that has all 1000 labels
+# with open('imagenet_classes.txt') as f:
+#     labels=[line.strip() for line in f.readlines()]
+
+# #find out index where max score output vector occur, this is the index that will be used to make the prediction
+# _ , index = torch.max(out,1)
+# percentage = torch.nn.functional.softmax(out,dim=1)[0]*100
+
+# print(labels[index[0]],percentage[index[0]].item())
+
+
+# #see what other classes the model thought the image belonged to
+# _, indices = torch.sort(out, descending=True)
+# print([(labels[idx], percentage[idx].item()) for idx in indices[0][:5]])
+
+
 
