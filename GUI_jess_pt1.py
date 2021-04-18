278a870ebc4e6b4e55f8b01874fd37a3dbe9de7a
selinbaydar <66303385+selinbaydar@users.noreply.github.com>
2021-04-18 18:30:24 -0400

    debugging GUI

--- GUI_jess_pt1.py
+++ GUI_jess_pt1.py
@@ -7,12 +7,16 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from torchvision import transforms
-import tkinter
+import tkinter as tk
 # import ipdb;ipdb.set_trace() #setting a breakpoint
 #modified tutorial code for this porject 
 
 #initialize window
-top = tkinter.Tk()
+top = tk.Tk()
+
+#TO DO: we can add window title
+#top.title('')
+
 #define window size
 top.geometry("1000x1000+1000+1000")
 # what is top??
@@ -32,34 +36,50 @@ transform = transforms.Compose([transforms.Resize(240),transforms.CenterCrop(224
 image1 = transform(image1)
 
 test = ImageTk.PhotoImage(image1)
-label1 = tkinter.Label(top,image=test)
+label1 = tk.Label(top,image=test)
 label1.image = test
 
 # Position image
 label1.place(x = 400, y= 40)
 # top.mainloop()
-# to do:do we want to include the image in the GUI - if so...https://www.activestate.com/resources/quick-reads/how-to-add-images-in-tkinter/
 
+#to get a general understanding of how to work with checkboxes
+#this code changes the text based on the option chosen
+#we will want to pre-code which option should be true
+
+# def print_selection():
+#     if (var1.get() == 1) & (var2.get() == 0):
+#         l.config(text='I love Python ')
+#     elif (var1.get() == 0) & (var2.get() == 1):
+#         l.config(text='I love C++')
+#     elif (var1.get() == 0) & (var2.get() == 0):
+#         l.config(text='I do not anything')
+#     else:
+#         l.config(text='I love both')
+
+#this is from here:https://pythonbasics.org/tkinter-checkbox/
 
 #how many variables/options want to have 
-CheckVar1 = IntVar()
-CheckVar2 = IntVar()
-CheckVar3 = IntVar()
-CheckVar4 = IntVar()
+var1 = tk.IntVar()
+var2 = tk.IntVar()
+var3 = tk.IntVar()
+var4 = tk.IntVar()
 
 #define visual aspects of it (master, option....)
 
+#need to define the option as a function so that it will change based on the image that is shown
+#create an answer key for the options
 
-C1 = Checkbutton(top, text = "option 1", variable = CheckVar1, \
+C1 = tk.Checkbutton(top, text = "option 1", variable = var1, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 10,)
-C2 = Checkbutton(top, text = "option 2", variable = CheckVar2, \
+C2 = tk.Checkbutton(top, text = "option 2", variable = var2, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 10)
-C3 = Checkbutton(top, text = "option 3", variable = CheckVar3, \
+C3 = tk.Checkbutton(top, text = "option 3", variable = var3, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 10)
-C4 = Checkbutton(top, text = "option 4", variable = CheckVar4, \
+C4 = tk.Checkbutton(top, text = "option 4", variable = var4, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 10)
 C1.pack()
@@ -68,3 +88,18 @@ C3.pack()
 C4.pack()
 
 top.mainloop()
+
+# def retrieve_input():
+#     global checkbox_response1
+#     global checkbox_response2
+#     global checkbox_response3
+#     global checkbox_response4
+#     checkbox_response1 = C1.get()
+#     checkbox_response2 = C2.get()
+#     checkbox_response3 = C3.get()
+#     checkbox_response4 = C4.get()
+
+#maybe we can add quit function once they get the correct response- but in that case would the matrix size not align?
+
+
+
