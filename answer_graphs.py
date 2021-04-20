c9193bdc63e9811bec766463bfe97fa734576c7b
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-20 17:58:06 -0400

    working on graphs, still need to resize and fix labels

--- answer_graphs.py
+++ answer_graphs.py
@@ -8,6 +8,12 @@ import pandas as pd
 import numpy as np
 #import numpy as np
 alex_sl = np.random.randint(2, size=[10,19])
+squeeze_sl = np.random.randint(2, size=[10,19])
+resnet_sl = np.random.randint(2, size=[10,19])
+vgg_sl = np.random.randint(2, size=[10,19])
+dense_sl = np.random.randint(2, size=[10,19])
+user_sl = np.random.randint(2, size=[10,19])
+
 #(alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
 #user_ans = np.ones(shape = [10,19])
 # def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl):
@@ -15,14 +21,44 @@ alex_sl = np.random.randint(2, size=[10,19])
     
 # # sum the columns/number of images--> create list
 # # sum the columns of alex_sl
-alex_c1=np.sum(alex_sl, axis=1)
+alex_c1=np.sum(alex_sl, axis=0)
+squeeze_c1=np.sum(squeeze_sl,axis=0)
+resnet_c1=np.sum(resnet_sl,axis=0)
+vgg_c1=np.sum(vgg_sl,axis=0)
+dense_c1=np.sum(dense_sl,axis=0)
+user_c1=np.sum(user_sl,axis=0)
 
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-names = ['Cheeseburger', 'Labrador_retriever', 'jeep', 'laptop', 'mushroom', 'pillow', 'sunglasses', 'tiger', 'mud_turtle', 'waffle_iron'] 
-model_ans = alex_c1
-ax.bar(names,model_ans)
+names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12','13','14','15','16','17','18','19'] 
+
+plt.subplot(3,3,1)
+plt.bar(names,alex_c1,0.5,color='grey',edgecolor='cyan')
+plt.xlabel('Crop Level')
 plt.title('Alex')
-plt.xlabel('images')
-plt.ylabel('Number correct per zoom level')
+
+plt.subplot(3,3,2)
+plt.bar(names,squeeze_c1,0.5,color='grey',edgecolor='cyan')
+plt.xlabel('Crop Level')
+plt.title('Squeeze')
+
+plt.subplot(3,3,3)
+plt.bar(names,resnet_c1,0.5,color='grey',edgecolor='cyan')
+plt.xlabel('Crop Level')
+plt.title('Resnet')
+
+plt.subplot(3,3,4)
+plt.bar(names,vgg_c1,0.5,color='grey',edgecolor='cyan')
+plt.xlabel('Crop Level')
+plt.title('VGG')
+
+plt.subplot(3,3,5)
+plt.bar(names,dense_c1,0.5,color='grey',edgecolor='cyan')
+plt.xlabel('Crop Level')
+plt.title('Dense')
+
+plt.subplot(3,3,6)
+plt.bar(names,user_c1,0.5,color='cyan', edgecolor='black')
+plt.xlabel('Crop Level')
+plt.title('YOU! (the user)')
+
 plt.show()
+
