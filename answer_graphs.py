bf7ce45987cc6510c80614335ac10a9ca4a1e1fc
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-20 10:00:02 -0400

    graphs

--- answer_graphs.py
+++ answer_graphs.py
@@ -1,49 +1,28 @@
-# # import required packages
-# import seaborn as sns 
-# import matplotlib.pyplot as plt
-# import pandas as pd 
-# import numpy as np
-
-#from zoom_game import y 
-#from GUI_mainscript import z
- #output of zoom_game
-#[False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, True, True, True, True, False, False, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-#[False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, False, True, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, True, False, False, True, False, False, False, False, False, False, False, False, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-#[False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, True, True, False, True, True, False, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, True, True, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-#[False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, True, True, True, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-#?[False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, True, True, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, True, True, False, True, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, True, False, False, False, True, True, True, True, True, False, True, False, False, False, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
-
-
-# # compare the 6 boolean matrixes that correspond to which answers are T/F
-# from zoom_game import zoom_me
-# import seaborn as sns 
-# import matplotlib.pyplot as plt
-# import pandas as pd 
-# #import numpy as np
-# (alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
-# def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl)
+# code written, debugged and tested by EJR
+# compare the 6 boolean matrixes that correspond to which answers are T/F
+#from zoom_game import zoom_me
+import seaborn as sns 
+import matplotlib.pyplot as plt
+import pandas as pd 
+#from GUI_jess_pt1 import user_ans
+import numpy as np
+#import numpy as np
+alex_sl = np.random.randint(2, size=[10,19])
+#(alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
+#user_ans = np.ones(shape = [10,19])
+# def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl):
 #     #extract minimum indices per row
     
+# # sum the columns/number of images--> create list
+# # sum the columns of alex_sl
+alex_c1=np.sum(alex_sl, axis=1)
 
-
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-sns.set()
-
-# Create example DataFrame of stock values
-data_frame = pd.DataFrame({
-    'ticker':np.repeat( ['aapl','goog','yhoo','msft'], 25 ),
-    'date':np.tile( pd.date_range('1/1/2011', periods=25, freq='D'), 4 ),
-    'price':(np.random.randn(100).cumsum() + 10) })
-
-data_frame['split'] = False
-data_frame['split 2'] = "No"
-
-
-plt.figure()
-sns.scatterplot(x=data_frame.date.dt.strftime('%Y-%w'), y='price', data=df, hue='split')
-plt.figure()
-sns.scatterplot(x=data_frame.date.dt.strftime('%Y-%w'), y='price', data=df, hue='split 2')
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+names = ['Cheeseburger', 'Labrador_retriever', 'jeep', 'laptop', 'mushroom', 'pillow', 'sunglasses', 'tiger', 'mud_turtle', 'waffle_iron'] 
+model_ans = alex_c1
+ax.bar(names,model_ans)
+plt.title('Alex')
+plt.xlabel('images')
+plt.ylabel('Number correct per zoom level')
+plt.show()
