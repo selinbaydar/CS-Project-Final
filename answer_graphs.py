c685e2aa2764947d451aa75c7a8ffedc17dd60dc
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-20 08:35:24 -0400

    potential graphs

--- answer_graphs.py
+++ answer_graphs.py
@@ -1,8 +1,8 @@
-# import required packages
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import pandas as pd 
-import numpy as np
+# # import required packages
+# import seaborn as sns 
+# import matplotlib.pyplot as plt
+# import pandas as pd 
+# import numpy as np
 
 #from zoom_game import y 
 #from GUI_mainscript import z
@@ -14,13 +14,36 @@ import numpy as np
 #?[False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, True, True, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, True, True, False, True, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False, False, False, True, False, False, False, True, True, True, True, True, False, True, False, False, False, True, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
 
 
-# compare the 6 boolean matrixes that correspond to which answers are T/F
-from zoom_game import zoom_me
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import pandas as pd 
-#import numpy as np
-(alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
-def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl)
-    #extract minimum indices per row
+# # compare the 6 boolean matrixes that correspond to which answers are T/F
+# from zoom_game import zoom_me
+# import seaborn as sns 
+# import matplotlib.pyplot as plt
+# import pandas as pd 
+# #import numpy as np
+# (alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
+# def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl)
+#     #extract minimum indices per row
     
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set()
+
+# Create example DataFrame of stock values
+data_frame = pd.DataFrame({
+    'ticker':np.repeat( ['aapl','goog','yhoo','msft'], 25 ),
+    'date':np.tile( pd.date_range('1/1/2011', periods=25, freq='D'), 4 ),
+    'price':(np.random.randn(100).cumsum() + 10) })
+
+data_frame['split'] = False
+data_frame['split 2'] = "No"
+
+
+plt.figure()
+sns.scatterplot(x=data_frame.date.dt.strftime('%Y-%w'), y='price', data=df, hue='split')
+plt.figure()
+sns.scatterplot(x=data_frame.date.dt.strftime('%Y-%w'), y='price', data=df, hue='split 2')
