7101d130fec777a9e5ff065d50269cb624cc8cd2
jessojeda <jessica_ojeda@brown.edu>
2021-04-20 23:43:48 -0400

    options GUI works! JO - 1hr

--- GUI_jess_pt1.py
+++ GUI_jess_pt1.py
@@ -7,6 +7,7 @@ from tkinter import messagebox as mb
 import tkinter as tk 
 import numpy as np
 
+
 def onSelect():
 
     #print(var1.get())
@@ -14,10 +15,9 @@ def onSelect():
     var1.set(0)
     #names each checkbutton, will change as GUI loops through
     #right now will show correct options AFTER clicking submit, need to set the first ones?
-    T1.set(option1)
-    T2.set(option2)
-    T3.set(option3)
-    T4.set(option4)
+
+    #need to make it so labels[x] turns into new one each time image_counter increases 
+
     my_img = "dog.jpg"
     left_box=190
     upper_box=190
@@ -27,7 +27,26 @@ def onSelect():
     tim = ImageTk.PhotoImage(im)
     label1.configure(image=tim)
     label1.image = tim
-<<<<<<< HEAD
+    #keeps track of how many times submit is pressed
+    onSelect.counter +=1
+    image_counter = onSelect.counter
+
+    print(image_counter)
+    with open('MC_options.txt') as f: #opens the textfile with the MC option names
+        labels=[line.strip() for line in f.readlines()]
+        #option1 = str(labels[4])
+        option1 = str(labels[(4*image_counter)])
+        #print(option1) - prints out first line 
+        #option2 = str(labels[5])
+        option2 = str(labels[4*image_counter+1])
+        #option3 = str(labels[6])
+        option3 = str(labels[4*image_counter+2])
+        #option4 = str(labels[7])
+        option4 = str(labels[4*image_counter+3])
+    T1.set(option1)
+    T2.set(option2)
+    T3.set(option3)
+    T4.set(option4)
     # if the answer is wrong for a certain image, change variables:             
     #         left_box = left_box - 10
     #         upper_box = upper_box - 10
@@ -36,24 +55,18 @@ def onSelect():
     #         zoom_level = zoom_level +1
     # use my_img=crop_me("dog.jpg",left_box,upper_box,right_box,lower_box)
     # user_ans[image_counter,zoom_level] = False
-=======
-    #if correct .. escape to next image 
-    # else
-
-#
->>>>>>> 89d2d577900643152bea614654cb967c9e71869c
-
+onSelect.counter = 0 
 # #track of image num 1-10
-image_counter = 0 
+
 # #track zoom level 1-15
 zoom_level = 0
 # #initialize matrix of user answers all listed as TRUE, which will later be replaced with FALSE if user is wrong
 user_ans = np.ones(shape = [10,19])
 
-with open('MC_options.txt') as f: #opens the textfile with the MC option names
+'''with open('MC_options.txt') as f: #opens the textfile with the MC option names
     labels=[line.strip() for line in f.readlines()]
-    option1 = str(labels[4])
-    #option1 = str(labels[image_counter+4])
+    #option1 = str(labels[4])
+    option1 = str(labels[image_counter+4])
     #print(option1) - prints out first line 
     option2 = str(labels[5])
     #option2 = str(labels[image_counter+3])
@@ -61,9 +74,9 @@ with open('MC_options.txt') as f: #opens the textfile with the MC option names
     #option3 = str(labels[image_counter+2])
     option4 = str(labels[7])
     #option4 = str(labels[image_counter+1])
-    #need to make it so labels[x] turns into new one each time image_counter increases 
+    #need to make it so labels[x] turns into new one each time image_counter increases '''
 
-def check_ans(image_counter):
+'''def check_ans(image_counter):
     #image counter still not working but when it does->
     #need something beforehand that goes through T1-T4 and outputs the string only if it was selected: 
     #if var 4==1: 
@@ -74,7 +87,7 @@ def check_ans(image_counter):
         image_counter = image_counter + 1 #moves image_counter to upload next question
     else:
         # change crop size
-
+'''
 # answer_opt_type is a txt file with the correct answers
 with open('answer_opt_type.txt') as g:
     answer_opt_type = [line.strip() for line in g.readlines()]
@@ -106,7 +119,13 @@ T1 = StringVar()
 T2 = StringVar()
 T3 = StringVar()
 T4 = StringVar()
-#T1.set('hello')
+#initial options that will show up, before onSelect
+T1.set('cheesburger')
+T2.set('light_bulb')
+T3.set('mouth')
+T4.set('garden_salad')
+
+
 C1 = tk.Checkbutton(top, textvariable = T1, variable = var1, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 10,)
