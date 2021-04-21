a423da3d773b02cf032dd584bdf96c1d2187ba30
jessojeda <jessica_ojeda@brown.edu>
2021-04-21 18:44:06 -0400

    JO - final changes to GUI

--- GUI_jess_pt1.py
+++ GUI_jess_pt1.py
@@ -9,69 +9,62 @@ import tkinter as tk
 import numpy as np
 from crop_img import crop_me
 
-left_box=150
-upper_box=150
-right_box=260
-lower_box=260
+#sets image size
+left_box=170
+upper_box=170
+right_box=230
+lower_box=230
 
 
 def onSelect():
-
     #keeps track of how many times submit is pressed
-    # onSelect.counter +=1
-    # to do: make image_coutner dependent on True or False answer, not on if button is pressed?
-    image_counter = 1
-    #image_counter = 1
-    with open('image_names.txt') as f: #opens the textfile with the MC option names
+    onSelect.counter += 1
+    image_counter = onSelect.counter
+    #opens the textfile with the image names to sort through images
+    with open('image_names.txt') as f: 
         labels=[line.strip() for line in f.readlines()]
-        #print(labels[0])
-        #will change which image file depending on correct count
+        #will change which image file opens depending on image_counter
         name = str(labels[(image_counter)])
-
     #sets the image
     my_img = name
     im = Image.open(my_img)#variable
+    #cropped image
     im = crop_me(im,left_box,upper_box,right_box,lower_box)
     transform = transforms.Compose([transforms.Resize(240),transforms.CenterCrop(224)])
     im = transform(im)
     tim = ImageTk.PhotoImage(im)
     label1.configure(image=tim)
     label1.image = tim
-
-
-    with open('MC_options.txt') as f: #opens the textfile with the MC option names
+    #opens the textfile with the MC option names
+    with open('MC_options.txt') as f:
+        #reads lines
         labels=[line.strip() for line in f.readlines()]
-        #define options based off textfile
-        #option1 = str(labels[4])
+        #define options based off textfile, transforms into strings 
+        #button 1 
         option1 = str(labels[(4*image_counter)])
-        #print(option1) - prints out first line 
-        #option2 = str(labels[5])
+        #button 2
         option2 = str(labels[4*image_counter+1])
-        #option3 = str(labels[6])
+        #button 3
         option3 = str(labels[4*image_counter+2])
-        #option4 = str(labels[7])
+        #button 4
         option4 = str(labels[4*image_counter+3])
-    #names each checkbutton, will change as GUI loops through
+    #names each checkbutton as the option previously defined, will change as GUI loops through
     T1.set(option1)
     T2.set(option2)
     T3.set(option3)
     T4.set(option4)
-
+    #opens answer key 
     with open('GUI_answer.txt') as f:
+        #reads file
         labels=[line.strip() for line in f.readlines()]
+        #assings int value to text 
         correct_ans = int(labels[image_counter])
-        print(correct_ans)
-        correct = False
-        while not correct:
-            if var1.get() == correct_ans or var2.get() == correct_ans or var3.get() == correct_ans or var4.get() == correct_ans:
-                image_counter += 1
-                print(image_counter)
-                print("yay")
-                correct = True
-            else:
-                print('wrong')
-                onSelect()
-
+        #checks if answer if correct by calling variables (var) assigned to each checkbutton and comparing to answer key
+        #each var has unique number associated, why can compare all at once
+        if var1.get() == correct_ans or var2.get() == correct_ans or var3.get() == correct_ans or var4.get() == correct_ans:
+            print("correct")
+        else:
+            print('wrong')
 
     #unselects answers after submit button pressed
     var1.set(0)
@@ -79,53 +72,24 @@ def onSelect():
     var3.set(0)
     var4.set(0)
 
-    # if the answer is wrong for a certain image, change variables:             
-    #         left_box = left_box - 10
-    #         upper_box = upper_box - 10
-    #         right_box = right_box + 10
-    #         lower_box = lower_box + 10 
-    #         zoom_level = zoom_level +1
-    # use my_img=crop_me("dog.jpg",left_box,upper_box,right_box,lower_box)
-    # user_ans[image_counter,zoom_level] = False
+#initialization
 onSelect.counter = 0 
-# #track of image num 1-10
-# #track zoom level 1-15
-zoom_level = 0
-# #initialize matrix of user answers all listed as TRUE, which will later be replaced with FALSE if user is wrong
-user_ans = np.ones(shape = [10,19])
-
-
-'''def check_ans(image_counter):
-    #image counter still not working but when it does->
-    #need something beforehand that goes through T1-T4 and outputs the string only if it was selected: 
-    #if var 4==1: 
-    #if T4 == answer_opt_type[image_counter]
-    #mayble loop though them 
-    if ....get() == answer_opt_type[image_counter]:
-        #if string of option selected = answer_opt_type of image 
-        image_counter = image_counter + 1 #moves image_counter to upload next question
-    else:
-        # change crop size
-'''
-# answer_opt_type is a txt file with the correct answers
-with open('answer_opt_type.txt') as g:
-    answer_opt_type = [line.strip() for line in g.readlines()]
-
-
 
 #initialize window
 top = tk.Tk()
 top.geometry("1000x1000+1000+1000")
 text = Text(top)
-# to do: maybe change to labels
+#initialize text instructions
 text.insert(INSERT, "Please identify the image. ")
 text.insert(END, "Select only one option. ")
 text.pack()
 
+#initalize the image first seen, cropped version
 image1 = Image.open("burger.jpg")
+image1 = crop_me(image1,left_box,upper_box,right_box,lower_box)
 transform = transforms.Compose([transforms.Resize(240),transforms.CenterCrop(224)])
 image1 = transform(image1)
-
+#places images in correct location
 test = ImageTk.PhotoImage(image1)
 label1 = tk.Label(top,image=test)
 label1.image = test
@@ -137,6 +101,7 @@ var2 = tk.IntVar()
 var3 = tk.IntVar()
 var4 = tk.IntVar()
 
+#variables to assign string to checkbuttons
 T1 = StringVar()
 T2 = StringVar()
 T3 = StringVar()
@@ -147,7 +112,7 @@ T2.set('light_bulb')
 T3.set('mouth')
 T4.set('garden_salad')
 
-
+#sets the checkbuttons
 C1 = tk.Checkbutton(top, textvariable = T1, variable = var1, \
                  onvalue = 1, offvalue = 0, height=5, \
                  width = 20,)
@@ -165,7 +130,9 @@ C2.pack()
 C3.pack()
 C4.pack()
 
+#sets the select button, calls on onSelect when select is pressed
 selectButton = tk.Button(top, text='Submit', command=onSelect)
 selectButton.pack(side = tk.LEFT)
 
+#opens GUI
 top.mainloop()
