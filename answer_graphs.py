1ea22b5d75fa38e4cfe6d14b862dd32a7457bcbf
jayjamer <79775288+jayjamer@users.noreply.github.com>
2021-04-21 21:59:55 -0400

    Update answer_graphs.py

--- answer_graphs.py
+++ answer_graphs.py
@@ -33,13 +33,7 @@ dense_sl = np.reshape(dense_sl,[10,19])
 #this is where we would input the user's performance on the zoom game. It is currently random numbers for the purpose of graphing
 user_sl = np.random.randint(2, size=[10,19])
 
-#(alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl) = zoom_me(my_directory)
-#user_ans = np.ones(shape = [10,19])
-# def compare_ans(user_ans,alex_sl,squeeze_sl,resnet_sl,vgg_sl,dense_sl):
-#     #extract minimum indices per row
-    
-# # sum the columns/number of images--> create list
-# # sum the columns of alex_sl
+# # sum the columns/sum across zoom levels
 alex_c1=np.sum(alex_sl, axis=0)
 squeeze_c1=np.sum(squeeze_sl,axis=0)
 resnet_c1=np.sum(resnet_sl,axis=0)
@@ -49,39 +43,49 @@ user_c1=np.sum(user_sl,axis=0)
 
 names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12','13','14','15','16','17','18','19'] 
 
+
+plt.suptitle('Number of images correct at each zoom level')
+plt.tight_layout(pad=300)
 plt.subplot(3,3,1)
 plt.bar(names,alex_c1,0.5,color='grey',edgecolor='cyan')
 plt.title('Alex')
-plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tick_params(axis='x', which='major', labelsize=0)
+plt.tight_layout(pad=300)
 
 plt.subplot(3,3,2)
 plt.bar(names,squeeze_c1,0.5,color='grey',edgecolor='cyan')
 plt.title('Squeeze')
-plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tick_params(axis='x', which='major', labelsize=0)
+plt.tight_layout(pad=300)
 
 plt.subplot(3,3,3)
 plt.bar(names,resnet_c1,0.5,color='grey',edgecolor='cyan')
 plt.title('Resnet')
-plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tick_params(axis='x', which='major', labelsize=0)
+plt.tight_layout(pad=300)
 
 plt.subplot(3,3,4)
 plt.bar(names,vgg_c1,0.5,color='grey',edgecolor='cyan')
 plt.title('VGG')
 plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tight_layout(pad=300)
 
 plt.subplot(3,3,5)
 plt.bar(names,dense_c1,0.5,color='grey',edgecolor='cyan')
 plt.title('Dense')
 plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tight_layout(pad=300)
 
 plt.subplot(3,3,6)
 plt.bar(names,user_c1,0.5,color='cyan', edgecolor='black')
 plt.title('YOU! (the user)')
+plt.tight_layout(pad=300)
 
 axes = plt.gca()
 axes.xaxis.label.set_size(20)
 axes.yaxis.label.set_size(20)
 plt.tick_params(axis='x', which='major', labelsize=5)
+plt.tight_layout(pad=300)
 
 plt.show()
 
